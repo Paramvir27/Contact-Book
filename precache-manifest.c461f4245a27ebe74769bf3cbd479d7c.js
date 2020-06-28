e6d948ba60795515d801e67caa1931583d62f973
Paramvir Singh <48600406+Paramvir27@users.noreply.github.com>
2020-06-28 22:05:53 +0530

    Add files via upload

---
+++ precache-manifest.c461f4245a27ebe74769bf3cbd479d7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd19716820ee99af34683e5dc344c5f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cbe658bc88b5afb09711",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "215632eb286d9c116e9c",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cbe658bc88b5afb09711",
+    "url": "./static/js/2.071ff71c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.071ff71c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "215632eb286d9c116e9c",
+    "url": "./static/js/main.f217a2db.chunk.js"
+  },
+  {
+    "revision": "d4d6723db7af89e76f0b",
+    "url": "./static/js/runtime-main.304a7af0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.38fc2edb3b8c513eb420069dfbbedd50.js"
+  "./precache-manifest.c461f4245a27ebe74769bf3cbd479d7c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
