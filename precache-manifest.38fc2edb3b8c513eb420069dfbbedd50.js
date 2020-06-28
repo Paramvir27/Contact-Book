be6d6b1767ee86d640666f25e2314bab5fd350f5
Paramvir Singh <48600406+Paramvir27@users.noreply.github.com>
2020-06-28 21:56:01 +0530

    Add files via upload

---
+++ precache-manifest.38fc2edb3b8c513eb420069dfbbedd50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea383c7790aee3e5bf35012ac670479",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbe658bc88b5afb09711",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1efcc75a06083557ce61",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cbe658bc88b5afb09711",
+    "url": "/static/js/2.071ff71c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.071ff71c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1efcc75a06083557ce61",
+    "url": "/static/js/main.32145943.chunk.js"
+  },
+  {
+    "revision": "022753f358e276ea7e82",
+    "url": "/static/js/runtime-main.5795e67e.js"
+  }
+]);
